c3791197123a950d75858ccbd84ecfaee4c849fe
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-04 20:07:29 +0300

    123

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,67 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+console.log(
+    galleryItems
+);
+
+const galleryContainer =
+    document.querySelector(
+        ".gallery"
+    );
+const galleryMarkup =
+    itemMarkup(
+        galleryItems
+    );
+
+galleryContainer.insertAdjacentHTML(
+    "afterbegin",
+    galleryMarkup
+);
+
+function itemMarkup(
+    galleryItems
+) {
+    return galleryItems
+        .map(
+            ({
+                preview,
+                original,
+                description,
+            }) => {
+                return `
+        <div class="gallery__item">
+          <a class="gallery__item" href="large-image.jpg">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </div>`;
+            }
+        )
+        .join("");
+}
+function openImgModal(
+    event
+) {
+    event.preventDefault();
+    if (
+        !event.target.classList.contains(
+            "gallery__image"
+        )
+    ) {
+        return;
+    }
+
+    const ImgUrlOriginal =
+        event.target
+            .dataset
+            .source;
+    const instance =
+        basicLightbox.create(`
+  <img src="${ImgUrlOriginal}" width="800" height="600"/>
+  `);
+    instance.show();
+}
+galleryContainer.addEventListener(
+    "click",
+    openImgModal
+);
