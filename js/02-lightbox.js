9092645cf56abc6485ab15842f67b5b24ba3ccf0
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-05 21:52:54 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,61 +1,22 @@
 import { galleryItems } from "./gallery-items.js";
 
-const galleryContainer =
-    document.querySelector(
-        ".gallery"
-    );
-const galleryMarkup =
-    itemMarkup(
-        galleryItems
-    );
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = itemMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML(
-    "afterbegin",
-    galleryMarkup
-);
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
 
-function itemMarkup(
-    galleryItems
-) {
+function itemMarkup(galleryItems) {
     return galleryItems
-        .map(
-            ({
-                preview,
-                original,
-                description,
-            }) => {
-                return `
-        <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
-            }
-        )
+        .map(({ preview, original, description }) => {
+            return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+        })
         .join("");
 }
-
-function openImgModal(
-    event
-) {
-    event.preventDefault();
-    if (
-        !event.target.classList.contains(
-            "gallery__image"
-        )
-    ) {
-        return;
-    }
-
-    new SimpleLightbox(
-        ".gallery a",
-        {
-            captionData:
-                "alt",
-            captionDelay: 250,
-            animationSpeed: 250,
-        }
-    );
-}
-galleryContainer.addEventListener(
-    "click",
-    openImgModal
-);
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    animationSpeed: 250,
+});
